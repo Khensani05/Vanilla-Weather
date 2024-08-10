2d35c0d534b44712a8b7027d2c0a26b57e667bef
Khensani05 <notigokhensani@gmail.com>
2024-08-10 16:45:51 +0200

    Made it display the weather condition, humidity, wind speed and date

--- src/index.js
+++ src/index.js
@@ -1,10 +1,53 @@
 function updateWeatherData(response) {
+  //display temperature
   let tempElement = document.querySelector("#weather-Temp");
   let temperature = response.data.temperature.current;
   tempElement.innerHTML = Math.round(temperature);
 
+  //display weather condtion
+  let weatherCondition = document.querySelector("#condition-type");
+  weatherCondition.innerHTML = response.data.condition.description;
+
+  //display the city name as in the api object
   let cityElement = document.querySelector("#show-City");
   cityElement.innerHTML = response.data.city;
+
+  //display humidity
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+
+  //display wind speed
+  let speedElement = document.querySelector("#speed");
+  speedElement.innerHTML = response.data.wind.speed;
+
+  //display the time
+  let date = new Date(response.data.time * 1000);
+  let timeElement = document.querySelector("#date");
+
+  let day = date.getDay();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes`0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {

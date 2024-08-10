1849d300f0700d7173c7ddf2ef31d9ffd14e62b3
Khensani05 <notigokhensani@gmail.com>
2024-08-10 15:41:40 +0200

    Added the weather functionality to display current temperature

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
+function updateWeatherData(response) {
+  let tempElement = document.querySelector("#weather-Temp");
+  let temperature = response.data.temperature.current;
+  tempElement.innerHTML = Math.round(temperature);
+
+  let cityElement = document.querySelector("#show-City");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "b03c74bff4oa46bc3d3tee6230146d23";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=b03c74bff4oa46bc3d3tee6230146d23&units=metric`;
+  console.log(apiKey);
+  console.log(apiUrl);
+  axios.get(apiUrl).then(updateWeatherData);
+}
+
 function changeCityName(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#enter-City");
-  console.log(searchInput.value);
-
-  let cityElement = document.querySelector("#show-City");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchElement = document.querySelector("#searchAnywhere");
-console.log(searchElement);
 searchElement.addEventListener("submit", changeCityName);
+
+searchCity("Johannesburg");
